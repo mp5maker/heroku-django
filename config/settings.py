da36b96d04596398a74513c8a0b7e4656f6f6ad2
Shabuktagin Photon Khan <khan.photon@gmail.com>
2019-05-01 22:48:25 +0600

    Debug turned true

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fxwtu5)joo#w9ft1t8!tnk9z&#8&l-ky^4$y35jg=fhjf0n#v$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
