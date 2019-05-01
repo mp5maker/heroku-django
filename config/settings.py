fba6cb606e417395a01f503f7925beac77bf9ccd
Shabuktagin Photon Khan <khan.photon@gmail.com>
2019-05-01 22:43:59 +0600

    Updated few settings for whitenoise

--- config/settings.py
+++ config/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages'
+    'pages',
 ]
 
 MIDDLEWARE = [
