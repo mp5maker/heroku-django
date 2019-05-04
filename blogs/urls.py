ec0d5715222dec9e6e8efc43d484d7beeb3fcc92
Shabuktagin Photon Khan <khan.photon@gmail.com>
2019-05-05 04:15:46 +0600

    Add Update View Page

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 
-from .views import BlogListPageView, BlogDetailPageView, BlogCreatePageView
+from .views import (
+    BlogListPageView,
+    BlogDetailPageView,
+    BlogCreatePageView,
+    BlogUpdatePageView
+)
 
 app_name = "blogs"
 
 urlpatterns = [
     path('',  BlogListPageView.as_view(), name="list"),
     path('create/',  BlogCreatePageView.as_view(), name="create"),
+    path('edit/<slug:slug>/',  BlogUpdatePageView.as_view(), name="update"),
     path('<slug:slug>/',  BlogDetailPageView.as_view(), name="details")
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from django.views.generic import ListView, DetailView
 
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 from .models import Post
 
@@ -18,3 +18,8 @@ class BlogCreatePageView(CreateView):
     template_name="blogs/create.html"
     model=Post
     fields=('title', 'author', 'body')
+
+class BlogUpdatePageView(UpdateView):
+    template_name="blogs/update.html"
+    model=Post
+    fields=('title', 'body', 'author')
