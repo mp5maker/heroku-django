5b239e4dc378eb72ab7015499d5f819a25b66a8f
Shabuktagin Photon Khan <khan.photon@gmail.com>
2019-05-05 20:03:54 +0600

    Added all the tests for create, update, delete, list page

--- blogs/tests.py
+++ blogs/tests.py
@@ -43,6 +43,11 @@ class PostModelTest(TestCase):
         self.assertEqual(post['created_at'], self.created_at)
         self.assertEqual(post['updated_at'], self.updated_at)
 
+    def test_get_absolute_url(self):
+        self.assertTrue(self.post.get_absolute_url(),
+            'blogs/' + self.slug + "/"
+        )
+
 
 class BlogListPageViewTest(TestCase):
     def setUp(self):
@@ -111,3 +116,75 @@ class BlogDetailPageViewTest(TestCase):
         self.assertContains(self.reverseUrl, 'testing the body')
         self.assertTrue(no_response, 404)
         self.assertTrue(no_response_reverse, 404)
+
+class BlogCreatePageViewTest(TestCase):
+    def setUp(self):
+        self.title ="this is just a test"
+        self.author = get_user_model().objects.create(
+            username="test",
+            email="test@gmail.com",
+            password="secret89"
+        )
+        self.body="great day"
+        self.response = self.client.post(reverse('blogs:create'), {
+            "title": self.title,
+            "body": self.body,
+            "author": self.author
+        })
+
+    def test_blog_create_page_view(self):
+        create_page_view = self.client.get(reverse('blogs:create'))
+        self.assertTrue(create_page_view.status_code, 200)
+        self.assertTemplateUsed(create_page_view, 'blogs/create.html')
+        self.assertTrue(self.client.get(reverse('blogs:create')).status_code, 200)
+        self.assertTrue(self.response.status_code, 200)
+        self.assertContains(self.response, self.title)
+        self.assertContains(self.response, self.body)
+
+class BlogUpdatePageViewTest(TestCase):
+    def setUp(self):
+        self.title = "this is just a test"
+        self.body = "boom shakalaka"
+        self.slug = slugify(self.title[:25])
+        self.author = get_user_model().objects.create(
+            username="test",
+            email="khan@gmail.com",
+            password="secret78"
+        )
+        self.post = Post.objects.create(
+            title=self.title,
+            body=self.body,
+            author=self.author
+        )
+        self.response = self.client.post(reverse('blogs:update',
+            kwargs={"slug" : self.slug}), {
+            "title": "this is updated test",
+            "body": "Boom has been eliminated"
+        })
+
+    def test_blog_update_page_view(self):
+        self.assertTrue(self.response.status_code, 302)
+        self.assertContains(self.response, 'this is updated test')
+        self.assertContains(self.response, 'Boom has been eliminated')
+
+class BlogDeletePageViewTest(TestCase):
+    def setUp(self):
+        self.title = "this is just a test"
+        self.body = "boom shakalaka"
+        self.slug = slugify(self.title[:25])
+        self.author = get_user_model().objects.create(
+            username="test",
+            email="khan@gmail.com",
+            password="secret78"
+        )
+        self.post = Post.objects.create(
+            title=self.title,
+            body=self.body,
+            author=self.author
+        )
+    def test_blog_delete_page_view(self):
+        delete_page = self.client.get(reverse('blogs:delete', kwargs={"slug": self.slug}))
+        response = self.client.post(reverse('blogs:delete', kwargs={"slug": self.slug}))
+        self.assertTrue(delete_page.status_code, 200)
+        self.assertTemplateUsed(delete_page, 'blogs/delete.html')
+        self.assertTrue(response.status_code, 302)
